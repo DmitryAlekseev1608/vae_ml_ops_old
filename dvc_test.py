4f899793a42729b455a694dc48baafab791febf2
Dmitry <a89652251224@gmail.com>
2023-12-21 14:42:13 +0300

    Stage

--- dvc_test.py
+++
@@ -1,6 +0,0 @@
-from dvc.api import DVCFileSystem
-
-
-url = "https://drive.google.com/drive/folders/1Lce2qugeTvVmcL_NzkLKfDQ0Y4h1nG6Z"
-fs = DVCFileSystem(url, rev="main")
-fs.get("data", "files", recursive=True)
